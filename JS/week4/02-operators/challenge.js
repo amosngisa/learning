1d947701db2b80f81678c3a730b82d5c8d6ed2c8
amos ngisa <amosnyaundi4@gmail.com>
2024-06-19 12:41:08 +0300

    challenge

--- JS/week4/02-operators/challenge.js
+++ JS/week4/02-operators/challenge.js
@@ -1,12 +1,25 @@
+
+const getMinMaxSum = (numbers) => {
+    //destructuring
+    const [first, ...rest] = [numbers];
+   
+    const {min,max,sum} = rest.reduce((acc,num) =>{
+        return{
+            min: Math.min(acc.min, num),
+            max: Math.max(acc.max, num),
+            sum: acc.sum + num
+        };
+
+    }, {min: first, max: first, sum: first});
+
+    return{min, max, sum};
+
+
+};
+
 const numbers = [1,2,3,4,5];
 
-let getMinMaxSum = (numbers) => {
-    let min = 1;
-    let max = 5;
-    let sum = 15;
+let result = getMinMaxSum(numbers)
 
-
-}
-
-console.log(getMinMaxSum);//{min: 1, max: 5, sum: 15}
+console.log(result);//{min: 1, max: 5, sum: 15}
 
