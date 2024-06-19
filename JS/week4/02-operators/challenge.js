1428c3554c9d2d5dde86678bfdf3c8eeaf59beef
amos ngisa <amosnyaundi4@gmail.com>
2024-06-19 12:11:18 +0300

    challenge

---
+++ JS/week4/02-operators/challenge.js
@@ -0,0 +1,12 @@
+const numbers = [1,2,3,4,5];
+
+let getMinMaxSum = (numbers) => {
+    let min = 1;
+    let max = 5;
+    let sum = 15;
+
+
+}
+
+console.log(getMinMaxSum);//{min: 1, max: 5, sum: 15}
+
