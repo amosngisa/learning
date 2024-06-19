59969376c9345bbfa4ec23975f64f82f5f1e75ea
amos ngisa <amosnyaundi4@gmail.com>
2024-06-19 10:17:43 +0300

    operators

---
+++ JS/week4/02-operators/destructing.js
@@ -0,0 +1,13 @@
+//syntax
+// const {variable1, variable2} = object;
+
+//Example
+const person = {name: 'Shawn Paul', age: 21}; //object
+
+const {name, age} = person; //variables
+
+console.log(name); //''Shawn Paul'
+
+console.log(age); // 21
+
+
