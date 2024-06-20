0fd975c0b3fbdcee2da636c810b3ec2794300707
amos ngisa <amosnyaundi4@gmail.com>
2024-06-20 08:46:29 +0300

    challenge

--- JS/week4/02-operators/challenge.js
+++ JS/week4/02-operators/challenge.js
@@ -1,8 +1,11 @@
 
 const getMinMaxSum = (numbers) => {
     //destructuring
-    const [first, ...rest] = [numbers];
+    //The numbers array is destructured. "first" is assigned the first element of the array, and rest is assigned the rest of the array elements.
+    // If numbers = [1, 2, 3, 4, 5], then first = 1 and rest = [2, 3, 4, 5]
+    const [first, ...rest] = numbers;
    
+
     const {min,max,sum} = rest.reduce((acc,num) =>{
         return{
             min: Math.min(acc.min, num),
@@ -12,8 +15,13 @@ const getMinMaxSum = (numbers) => {
 
     }, {min: first, max: first, sum: first});
 
+    //The reduce function is used to iterate over the rest array, updating the acc (accumulator) object 
+    //which keeps track of the current minimum (min), maximum (max), and sum (sum) of the array elements.
+    //For each num in rest, Math.min and Math.max functions update the min and max values respectively, and the sum is incremented by num.
+    
     return{min, max, sum};
 
+    //The function returns an object with the computed min, max, and sum values
 
 };
 
