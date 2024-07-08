225f3a5c42daf8ea04a901caa75137d2ea52ffb3
amos ngisa <amosnyaundi4@gmail.com>
2024-07-08 13:00:11 +0300

    Hooks in react

--- React/week1/01-Introduction/my-app/src/main.jsx
+++ React/week1/01-Introduction/my-app/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Reducer'
+import App from './Callback'
 
 // //import RickyAndMorty from './Hook'
 // import {BrowserRouter} from 'react-router-dom'//react-router-dom import
