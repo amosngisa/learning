9aec662837d39213c72ff894457996f0d5dc756a
amosngisa <amosnyaundi4@gmail.com>
2024-08-12 12:20:54 +0300

    Tailwind CSS

--- Python/Django/myapp/views.py
+++ Python/Django/myapp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     context = {'message': 'Hello world', 'text':'Welcome to my page. Horaaaay!'}
+    # user = {'username': 'Amos', 'authenticated': True, 'items': ['rabbit', 'cats']}
     return render(request, 'index.html', context)
 
 def about(request):
