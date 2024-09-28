e42a898a63033aa774363037b86c526c45ce7240
amosngisa <amosnyaundi4@gmail.com>
2024-09-28 13:02:57 +0300

    sorting

--- DSA/sorting/selection.py
+++ DSA/sorting/selection.py
@@ -1,21 +1,34 @@
-import random
+# import random
 
-n = 6
-# # generate random array
-arr = random.sample(range(1, 100), n)
+# n = 6
+# # # generate random array
+# arr = random.sample(range(1, 100), n)
 
-# n = len(arr)
+# # n = len(arr)
+# def selection_sort(arr):
+#     for i in range(n):
+#         minimum = i
+#         for j in range(i+1, n):
+#             if arr[j] < arr[minimum]:
+#                 minimum = j
+#         arr[i], arr[minimum] = arr[minimum], arr[i]
+        
+#         print(arr)
+#     return arr
+    
+# print("Sorted array: ", selection_sort(arr))
+    
+arr = [29,10,14,37,13]
+
 def selection_sort(arr):
+    n = len(arr)
     for i in range(n):
         minimum = i
         for j in range(i+1, n):
             if arr[j] < arr[minimum]:
                 minimum = j
         arr[i], arr[minimum] = arr[minimum], arr[i]
-        
         print(arr)
     return arr
-    
-print("Sorted array: ", selection_sort(arr))
-    
-array = [29,10,14,37,13]
+        
+print(selection_sort(arr))
